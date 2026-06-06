6df48334d08d26606845c9882e127c15fc886eb0
tkirtman <kirtmata@mail.uc.edu>
2026-06-06 18:06:23 -0400

    added pseudo code comments

--- src/ShipCostCalculator.java
+++ src/ShipCostCalculator.java
@@ -1,27 +1,31 @@
 void main() {
+//initialized variables
         double itemPrice = 0.0;
         double shippingRate = 0.02;
         double shippingCost = 0.0;
         double totalCost;
         String badInput = "";
-
+//print instructions
     System.out.println("Enter the cost of your item: ");
+    //create scanner
     Scanner in = new Scanner(System.in);
+    // check to see if itemPrice is double
     if (in.hasNextDouble()){
+        // set itemPrice
         itemPrice = in.nextDouble();
-
-        in.close();
-
+// calculate shipping costs
         if (itemPrice >= 100.00){
             totalCost = itemPrice;
         } else {
             shippingCost = shippingRate * itemPrice;
             totalCost = shippingCost + itemPrice;
         }
+        // print output
         System.out.println("The shipping cost is: $" + shippingCost);
         System.out.println("The total price is: $" + totalCost);
         //scanner.nextLine();
     } else {
+        // print error message
         System.out.println("Please enter a valid number.");
         badInput = in.nextLine();
     }
